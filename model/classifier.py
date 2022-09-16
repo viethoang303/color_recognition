d0baa48737401cd7b975a58524693ce64ebb5763
tungedng2710 <tungnguyen99.tn@gmail.com>
2022-09-16 19:40:14 +0700

    update

--- model/classifier.py
+++ model/classifier.py
@@ -1,72 +1,122 @@
 from argparse import ArgumentParser
 
 import torch
+from torch import nn
 from torch.nn import functional as F
+
 import pytorch_lightning as pl
 import torchmetrics
-from torchmetrics import F1Score
+from torchmetrics import F1Score, ConfusionMatrix
+import torchvision
+import clip
 
-from model import AlexNet
+from collections import OrderedDict
 
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class VehicleClassifier(pl.LightningModule):
-    def __init__(self, n_classes=15, learning_rate=1e-3):
+    def __init__(self, n_classes=14, learning_rate=1e-3):
         super().__init__()
         self.save_hyperparameters()
-        self.backbone = AlexNet()
+        self.n_classes = n_classes
         self.lr = learning_rate
-        # self.train_acc = torchmetrics.Accuracy()
-        # self.val_acc = torchmetrics.Accuracy()
-        # self.test_acc = torchmetrics.Accuracy()
-
         self.f1_score = F1Score(num_classes = n_classes)
+
+        # self.backbone = torchvision.models.efficientnet_v2_s()
+        # self.backbone.load_state_dict(torch.load('pretrained_checkpoint_model/efficientnet_v2_s-dd5fe13b.pth'))
+        # self.model, preprocess = clip.load(clip.load("ViT-L/14", device="cuda"))
+        # self.model.float()
+
+        # self.model = torchvision.models.vit_b_16(pretrained=False)
+        # self.model.load_state_dict(torch.load('vit_b_16_imagenet.pt'))
+        # modules = list(self.model.children())[:-1]
+        # self.model = nn.Sequential(*list(modules))
+
+        self.model = torchvision.models.efficientnet_v2_s(num_classes=n_classes)
+        # self.model.load_state_dict(torch.load("pretrained_checkpoint_model/efficientnet_v2_s-dd5fe13b.pth"))
+        # modules = list(self.model.children())[:-1]
+        # self.model = nn.Sequential(*list(modules))
+        # self.gelu = nn.GeLU(approximate="none")   
+        # self.fc1 = nn.Linear(1280, n_classes, bias=True)
+        # self.model = torchvision.models.resnet50(num_classes=n_classes)
 
 
     def forward(self, x):
-        out = self.backbone(x)
+        # if self.current_epoch < 15:
+        #     with torch.no_grad():
+        #         out = self.model(x)
+        # else:
+        out = self.model(x)
+        
+        # out = out.squeeze(dim=2).squeeze(dim=2)
+        # out = self.fc1(out)
         return out
     
     def training_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_hat = self.backbone(x.float())
+        y_hat = self.forward(x.float())
         loss = F.cross_entropy(y_hat, y)
 
         # class_pred = torch.max(y_hat.detach(), dim=1)[1]
         acc = self.f1_score(y_hat, y)
         self.log('train_loss ', loss)
-        self.log('train_acc', acc)
+        self.log('train_f1_score', acc)
 
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_hat = self.backbone(x.float())
+        y_hat = self.forward(x.float())
         loss = F.cross_entropy(y_hat, y)
         acc = self.f1_score(y_hat, y)
 
         self.log('val_loss', loss)
-        # self.log('val_accuracy', acc)
+        self.log('val_f1_score', acc)
 
-        return {'loss': loss, 'val_acc': acc}
+        return {'loss': loss, 'val_f1_score': acc}
     
     def test_step(self, batch, batch_idx):
         x, y, _ = batch
-        y_hat = self.backbone(x.float())
+        y_hat = self.forward(x.float())
         loss = F.cross_entropy(y_hat, y)
         acc = self.f1_score(y_hat, y)
+
         
         self.log('test_loss', loss)
-        self.log('test_acc', acc)
+        self.log('test_f1_score', acc)
 
-        return {'loss': loss, 'accuracy': acc}
+        return {'loss': loss, 'test_f1_score': acc, 'y_hat': y_hat, 'y': y} 
+
+    # def test_epoch_end(self, outputs):
+    #     preds = torch.cat([tmp['y_hat'] for tmp in outputs])
+    #     targets = torch.cat([tmp['y'] for tmp in outputs])
+
+    #     confusion_matrix = torchmetrics.functional.classification.multiclass_confusion_matrix(preds.cpu(), targets.cpu(), self.n_classes)
+    #     self.log('confusion_matrix', confusion_matrix)
+        # df_cm = pd.DataFrame(confusion_matrix.numpy(), index = range(self.n_classes), columns=range(self.n_classes))
+        # plt.figure(figsize = (10,7))
+        # fig_ = sns.heatmap(df_cm, annot=True, cmap='Spectral').get_figure()
+        # plt.close(fig_)
+        
+        # self.logger.experiment.add_figure("Confusion matrix", fig_)
         
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.lr)
+        # optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, momentum=0.9, weight_decay=0.00001)
+        # lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.25, patience=0)
+        optimizer = torch.optim.Adamax(self.parameters(), lr=self.lr)   
+        # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, last_epoch=15)
+        return optimizer#{"optimizer": optimizer, "lr_scheduler": lr_scheduler, "monitor": "val_f1_score"}#torch.optim.Adam(self.parameters(), lr=self.lr)
+        # optimizer = torch.optim.RMSprop(self.parameters(), lr=self.lr, weight_decay=0.9, momentum=0.9, eps=0.001)
     
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
         parser.add_argument(
-            '--learning_rate', type=float, default=0.0001, required=False
+            '--learning_rate', type=float, default=1e-3, required=False
         )
         return parser
+
+if __name__ == "__main__":
+    model = VehicleClassifier(n_classes=14)
+    print(model)
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import torchvision
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.profiler import AdvancedProfiler
+from pytorch_lightning.callbacks import LearningRateMonitor
 
 from data_loader import VehicleDataModule, get_map_classification
 from model import VehicleClassifier
@@ -23,10 +24,12 @@ def cli_main():
     parser = ArgumentParser()
     parser.add_argument('--batch_size', default=32, type=int)
     parser.add_argument('--num_workers', default=8, type=int)
-    parser.add_argument('--n_classes', default=15, type=int)
-    parser.add_argument('--data_path', default='vehicle', type=str)
+    parser.add_argument('--n_classes', default=14, type=int)
+    parser.add_argument('--learning_rate', default=1e-3, type=float)
+    parser.add_argument('--data_path', default='color_data', type=str)
+    parser.add_argument('--device', default=0, type=int)
     parser = pl.Trainer.add_argparse_args(parser)
-    parser = VehicleClassifier.add_model_specific_args(parser)
+    # parser = VehicleClassifier.add_model_specific_args(parser)
     args = parser.parse_args()
 
     # ------------
@@ -38,34 +41,36 @@ def cli_main():
     class_map = get_map_classification('vehicle/train')
     class_map = {value: key for key, value in class_map.items()}
 
-    # model = VehicleClassifier(n_classes=data.n_classes)
-    model = torchvision.models.efficientnet_v2_s(num_classes=15)
+    model = VehicleClassifier(n_classes=data.n_classes, learning_rate=args.learning_rate)
+    
 
     checkpoint_callback = ModelCheckpoint(
         save_top_k = 1, 
-        monitor = 'val_acc',
+        monitor = 'val_f1_score',
         mode = 'max',
-        dirpath = 'checkpoints',
-        filename = "best-{epoch:02d}-{val_acc:.2f}"
+        dirpath = 'checkpoints/vit/new',
+        filename = "best-{epoch:02d}-{val_f1_score:.4f}"
     )
+
+    lr_monitor = LearningRateMonitor(logging_interval='step')
 
     trainer = pl.Trainer.from_argparse_args(
         args, 
         profiler=AdvancedProfiler(),
         callbacks = [
-            # ModelCheckpoint(save_top_k=1, monitor='val'),
-            checkpoint_callback
+            checkpoint_callback,
+            lr_monitor
         ],
         max_epochs=100, 
         accelerator='gpu', 
-        devices=[2]
+        devices=[args.device]
     )
     
-    # trainer.fit(model, data)
+    trainer.fit(model, data)
 
-    #Testing#
-    # result = trainer.test(model=model, dataloaders=data)#.test_dataloader(), ckpt_path = 'lightning_logs/version_0/checkpoints/epoch=99-step=11400.ckpt')
-    # print(result)
+    #Testing
+    result = trainer.test(model=model, dataloaders=data)#.test_dataloader(), ckpt_path = 'checkpoints/vit/new/best-epoch=56-val_f1_score=0.6907.ckpt')
+    print(result)
 
 if __name__ == '__main__':
     cli_main()
