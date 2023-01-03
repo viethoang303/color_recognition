f13cf7b7d61e594b97a563f02ac013ae2b910f34
Hoang <48119382+viethoang303@users.noreply.github.com>
2023-01-03 20:56:29 +0700

    Update classifier.py

--- model/classifier.py
+++ model/classifier.py
@@ -37,8 +37,8 @@ class VehicleClassifier(pl.LightningModule):
         self.save_hyperparameters()
         self.n_classes = n_classes
         self.lr = learning_rate
-        self.f1_score = F1Score(num_classes = n_classes)
-        self.acc = Accuracy(num_classes=n_classes) 
+        self.f1_score = F1Score(num_classes = n_classes, task="multiclass")
+        self.acc = Accuracy(num_classes=n_classes, task="multiclass") 
 
 #         self.backbone = torchvision.models.efficientnet_v2_s(pretrained=True)
         # self.backbone.load_state_dict(torch.load('pretrained_checkpoint_model/efficientnet_v2_s-dd5fe13b.pth'))
