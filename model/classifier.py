403139adfe8a6e6da7608127d01b51f9274f1617
Hoang <48119382+viethoang303@users.noreply.github.com>
2023-01-03 17:34:35 +0700

    Update classifier.py

--- model/classifier.py
+++ model/classifier.py
@@ -13,21 +13,22 @@ import torchvision
 
 from collections import OrderedDict
 from .attention import CBAM, BAM
-import m
+import math
 
 class WarmupLinearSchedule(LambdaLR):
     """ Linear warmup and then linear decay.
         Linearly increases learning rate from 0 to 1 over `warmup_steps` training steps.
         Linearly decreases learning rate from 1. to 0. over remaining `t_total - warmup_steps` steps.
     """
-    def __init__(self, optimizer, warmup_steps, t_total, last_epoch=-1):
+    def __init__(self, optimizer, warmup_steps, t_total, inital_lr, last_epoch=-1):
         self.warmup_steps = warmup_steps
         self.t_total = t_total
+        self.inital_lr = inital_lr
         super(WarmupLinearSchedule, self).__init__(optimizer, self.lr_lambda, last_epoch=last_epoch)
 
     def lr_lambda(self, step):
         if step < self.warmup_steps:
-            return float(step) / float(max(1, self.warmup_steps))
+            return self.inital_lr * float(step) / float(max(1, self.warmup_steps))
         return max(0.0, float(self.t_total - step) / float(self.t_total))
 
 class VehicleClassifier(pl.LightningModule):
